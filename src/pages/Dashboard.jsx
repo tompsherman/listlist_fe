ec523987c588f7d60ca30659d8490b8db1eaad26
unknown <tompsherman@gmail.com>
2022-04-18 09:30:47 -0400

    shopping display

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -36,8 +36,10 @@ const Dashboard = ({ getList }) => {
     axios
       // .post(`http://localhost:5505/api/lists/`, newGroceryList)
       .post(`https://listlesslist.heroku.com/api/lists/`, newGroceryList)
-      .then((response) => console.log("item response:", response))
-      .then(() => setShopping(true))
+      .then(
+        (response) => console.log("item response:", response),
+        setShopping(true)
+      )
       .catch((error) => console.log(error));
   };
   return (
